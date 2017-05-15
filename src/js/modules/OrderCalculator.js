48fbb53044c0bbdc2cd9cc546dd03cca8d9387c7
nigel-riss <mr.kurenkov@gmail.com>
2017-05-15 16:37:20 +0200

    Order calculator auto-pricing finished

--- src/js/modules/OrderCalculator.js
+++ src/js/modules/OrderCalculator.js
@@ -2,11 +2,29 @@ import $ from 'jquery';
 
 class OrderCalculator {
     constructor() {
+        this.countInputs = $('.table-item__count input');
+        this.costLabels = $('.table-item__cost');
+        this.summLabel = $('.table-item__summ');
         this.events();
     }
 
     events() {
-        alert('hello calc');
+        this.countInputs.on('change', function(event) {
+            this.calcTotal($(event.target));
+        }.bind(this));
+    }
+
+    calcTotal(input) {
+        let price = parseInt(input.parent().parent().children('.table-item__price').html());
+        let count = parseInt( input.val() );
+        let cost = input.parent().parent().children('.table-item__cost');
+        cost.html(price * count);
+
+        let summ = 0;
+        $.each(this.costLabels, function(i, val){
+            summ += parseInt($(val).html());
+        })
+        this.summLabel.html(summ);
     }
 }
 
