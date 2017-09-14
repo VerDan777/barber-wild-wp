210c1ca1d8f5d67974e1cf870be3dd3864c6884f
Daniil Vereschagin <vereshhagin98@mail.ru>
2017-09-14 15:39:06 +0200

    partners fixes

--- wp/page-partners.php
+++ wp/page-partners.php
@@ -99,16 +99,16 @@
             </header>
 
             <div class="partners">
-                <a class="partners__item" href="http://oldboybarbershop.com/"><img src="./img/partners/oldboy.png"></a>
-                <a class="partners__item" href="http://www.salonborodach.ru/"><img src="./img/partners/borodach.png"></a>
-                <a class="partners__item" href="http://barberceh.ru/"><img src="./img/partners/ceh.png"></a>
-                <a class="partners__item" href="http://barbershopmagnum.ru/"><img src="./img/partners/magnum.png"></a>
-                <a class="partners__item" href="#"><img src="./img/partners/usi.png"></a>
-                <a class="partners__item" href="#"><img src="./img/partners/borodise.png"></a>
-                <a class="partners__item" href="#"><img src="./img/partners/wrong-bar.png"></a>
-                <a class="partners__item" href="http://big-bro.pro"><img src="./img/partners/bigbro.png"></a>
-                <a class="partners__item" href="http://barberkontora.ru/"><img src="./img/partners/kontora.png"></a>
-                <a class="partners__item" href="http://barberfirma.ru/"><img src="./img/partners/firma.png"></a>
+                <a class="partners__item" href=""><img src="<?php bloginfo('stylesheet_directory'); ?>./img/partners/oldboy.png"></a>
+                <a class="partners__item" href="http://www.salonborodach.ru/"><img src="<?php bloginfo('stylesheet_directory'); ?>./img/partners/borodach.png"></a>
+                <a class="partners__item" href="http://barberceh.ru/"><img src="<?php bloginfo('stylesheet_directory'); ?>./img/partners/ceh.png"></a>
+                <a class="partners__item" href="http://barbershopmagnum.ru/"><img src="<?php bloginfo('stylesheet_directory'); ?>./img/partners/magnum.png"></a>
+                <a class="partners__item" href="#"><img src="<?php bloginfo('stylesheet_directory'); ?>./img/partners/usi.png"></a>
+                <a class="partners__item" href="#"><img src="<?php bloginfo('stylesheet_directory'); ?>./img/partners/borodise.png"></a>
+                <a class="partners__item" href="#"><img src="<?php bloginfo('stylesheet_directory'); ?>./img/partners/wrong-bar.png"></a>
+                <a class="partners__item" href="http://big-bro.pro"><img src="<?php bloginfo('stylesheet_directory'); ?>./img/partners/bigbro.png"></a>
+                <a class="partners__item" href="http://barberkontora.ru/"><img src="<?php bloginfo('stylesheet_directory'); ?>./img/partners/kontora.png"></a>
+                <a class="partners__item" href="http://barberfirma.ru/"><img src="<?php bloginfo('stylesheet_directory'); ?>./img/partners/firma.png"></a>
             </div>
 
             </div>
