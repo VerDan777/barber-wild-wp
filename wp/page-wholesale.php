fa2fddef277c7e772f1d98957e1eab799141e08e
nigel-riss <mr.kurenkov@gmail.com>
2017-09-28 17:05:27 +0200

    Index input added to order

--- wp/page-wholesale.php
+++ wp/page-wholesale.php
@@ -175,7 +175,7 @@
 </div>
 
 <!--<script src="<?php bloginfo('stylesheet_directory'); ?>/app.js"></script>-->
-<script src="<?php bloginfo('stylesheet_directory'); ?>/orders.js?v=2"></script>
+<script src="<?php bloginfo('stylesheet_directory'); ?>/orders.js?v=3"></script>
 
 <?php 
     get_footer();
